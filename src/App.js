275f948f8cce0d79dc4313cfee07cb7ced8ca5e0
Chris Power <cpow85@gmail.com>
2021-03-11 08:08:47 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import {useInterval} from "react-use";
+
+function ShitRain() {
+  const [emojisToRender, setEmojisToRender] = useState([{offset: 0, key: 0, emoji: ''}]);
+
+  useInterval(() => {
+    if (emojisToRender.length > 20) {
+      emojisToRender.shift();
+    }
+
+    const offset = Math.floor(Math.random() * 1000);
+    const key = offset + Math.random() * 1000000;
+    const emoji = '💩';
+
+    emojisToRender.push({offset, key, emoji});
+
+    setEmojisToRender([...emojisToRender]);
+  }, 100);
+
+  return (
+    <SuperContainer>
+      {emojisToRender.map(({key, emoji, offset}) => {
+        return (
+          <EmojiContainer key={key} offset={offset}>
+            {emoji}
+          </EmojiContainer>
+        );
+      })}
+    </SuperContainer>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ShitRain/>
+    </>
   );
 }
 
 export default App;
+
+const SuperContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+const EmojiContainer = styled.div`
+  @keyframes falldown {
+    0% { margin-top: 0; }
+    100% { margin-top: 600px; }
+  }
+  
+  position: absolute;
+  top: 80px;
+  left: ${props => props.offset}px;
+  
+  font-size: 48px;
+  animation-name: falldown;
+  animation-duration: 4s;
+`;
